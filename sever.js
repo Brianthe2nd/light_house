61c94f83653546b99c983a6c823dca3855637a6d
Brian Ngugi <143685881+Brianthe2nd@users.noreply.github.com>
2024-01-10 19:00:14 +0300

    Update sever.js

--- sever.js
+++ sever.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {main} = require('./light');
+const {main_2} = require('./light_2'); // Assuming you have a second 'main' function in 'light_2'
 const app = express();
 const port = 3000;
 
@@ -9,7 +10,6 @@ app.get('/', (req, res) => {
     return res.status(200).json({ message: 'The server is running' });
 });
 
-
 app.post('/audit', async (req, res) => {
     const { urls } = req.body;
 
@@ -17,11 +17,16 @@ app.post('/audit', async (req, res) => {
         return res.status(400).json({ error: 'Invalid request. Please provide an array of URLs.' });
     }
 
+    // Divide the URLs into two halves
+    const mid = Math.ceil(urls.length / 2); 
+    const urls1 = urls.slice(0, mid);
+    const urls2 = urls.slice(mid);
+
     try {
-        // const Queue = await import('p-queue');
-        const lighthouseReports = await main(urls);
-        console.log(lighthouseReports)
-        res.json({ lighthouseReports });
+        // Use Promise.all to run both functions concurrently
+        const [lighthouseReports1, lighthouseReports2] = await Promise.all([main(urls1), main_2(urls2)]);
+        console.log(lighthouseReports1, lighthouseReports2)
+        res.json({ lighthouseReports1, lighthouseReports2 });
     } catch (error) {
         console.error('Error processing audit request:', error);
         res.status(500).json({ error: 'Internal server error' });
