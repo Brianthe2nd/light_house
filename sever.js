22bfe35c0e9941042e2878b17573487fbad828eb
Brian Ngugi <143685881+Brianthe2nd@users.noreply.github.com>
2024-01-10 19:35:41 +0300

    Update sever.js

--- sever.js
+++ sever.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {main} = require('./light');
-const {main_2} = require('./light_2'); // Assuming you have a second 'main' function in 'light_2'
+// const {main_2} = require('./light_2'); // Assuming you have a second 'main' function in 'light_2'
 const app = express();
 const port = 3000;
 
@@ -24,11 +24,13 @@ app.post('/audit', async (req, res) => {
 
     try {
         // Use Promise.all to run both functions concurrently
-        const [lighthouseReports1, lighthouseReports2] = await Promise.all([main(urls1), main_2(urls2)]);
+        const lighthouseReports1 = main(urls)
+        // const [lighthouseReports1, lighthouseReports2] = await Promise.all([main(urls1), main_2(urls2)]);
         // console.log(`report 1 is ${lighthouseReports1}`)
         // console.log(`report 2 is ${lighthouseReports2}`)
         // console.log(lighthouseReports1, lighthouseReports2)
-        res.json({ lighthouseReports1, lighthouseReports2 });
+        // res.json({ lighthouseReports1, lighthouseReports2 });
+        res.json(lighthouseReports1)
     } catch (error) {
         console.error('Error processing audit request:', error);
         res.status(500).json({ error: 'Internal server error' });
