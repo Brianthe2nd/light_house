0a7bc9313ed7ae52353eb17fcf9da9f242acf92f
Brian Ngugi <143685881+Brianthe2nd@users.noreply.github.com>
2024-01-11 13:54:02 +0300

    Update light.js

--- light.js
+++ light.js
@@ -62,30 +62,37 @@ const fetchLighthouseReports = async (urls, apiKey, Queue) => {
 
     progressBar.start(totalRequests, 0);
 
-    const queue = new Queue({ concurrency: 25 });
+    const queue = new Queue({ concurrency: 30 });
 
     const reportPromises = validUrls.map(url => queue.add(() => fetchLighthouseReport(url, apiKey, progressBar, Queue)));
 
     const reports = await Promise.allSettled(reportPromises);
 
-    const fulfilledReports = reports.filter(result => result.status === 'fulfilled' && result.value !== null).map(result => result.value); // Filter out null values
+    const fulfilledReports = reports.filter(result => result.status === 'fulfilled').map(result => result.value);
 
     progressBar.stop();
     const end = performance.now();
     const totalTime = end - start;
     console.log(`Total time taken: ${totalTime} milliseconds`);
+
+    // Write data to JSON file asynchronously
+    const jsonFilePath = 'lighthouse_reports.json';
+    try {
+        await fs.writeFile(jsonFilePath, JSON.stringify(fulfilledReports, null, 2));
+        console.log(`Lighthouse reports written to ${jsonFilePath}`);
+    } catch (error) {
+        console.error(`Error writing to ${jsonFilePath}:`, error);
+    }
+
     return fulfilledReports;
 };
-
 
 const main = async (realUrls) => {
     const apiKey = 'AIzaSyAC2a5FRPuTu1TBCoauzicmTSRiuisI8xA';
 
     try {
         const Queue = await import('p-queue');
-        let lighthouseReports = await fetchLighthouseReports(realUrls, apiKey, Queue.default);  
-        lighthouseReports = lighthouseReports.filter(item => item !== null);
-        console.log(lighthouseReports)
+        const lighthouseReports = await fetchLighthouseReports(realUrls, apiKey, Queue.default);        
         return lighthouseReports
         // console.log('All Lighthouse reports have been saved in lighthouse_reports.json', lighthouseReports);
     } catch (error) {
